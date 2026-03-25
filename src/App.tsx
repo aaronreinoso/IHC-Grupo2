d8afee1074d02130af596e0752e49fa3e3eb4c01
Aaron Reinoso <reinosoaaron@gmail.com>
2026-03-25 12:02:36 -0500

    arreglo de path customizado

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ export default function App() {
         {/* ==========================================
             1. LAYOUT GLOBAL (Menú principal)
         ========================================== */}
-        <Route path="/" element={<Layout />}>
+        <Route path="/IHC-Grupo2/" element={<Layout />}>
           
           <Route index element={<Dashboard />} />
           
